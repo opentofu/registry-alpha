b247c71ac6985e93c2a6752135884a78e6cb5c9b
Marcin Wyszynski <marcin.pixie@gmail.com>
2023-09-07 17:47:54 +0200

    X-Ray tracing for modules and providers functions (#22)

--- lambda/internal/providers/versions.go
+++ lambda/internal/providers/versions.go
@@ -3,9 +3,11 @@ package providers
 import (
 	"context"
 	"fmt"
+	"strings"
+
+	"github.com/aws/aws-xray-sdk-go/xray"
 	"github.com/opentffoundation/registry/internal/github"
 	"github.com/shurcooL/githubv4"
-	"strings"
 )
 
 // GetVersions fetches and returns a list of available versions of a given  provider hosted on GitHub.
@@ -18,48 +20,51 @@ import (
 // - name: The name of the provider without the "terraform-provider-" prefix.
 //
 // Returns a slice of Version structures detailing each available version. If an error occurs during fetching or processing, it returns an error.
-func GetVersions(ctx context.Context, ghClient *githubv4.Client, namespace string, name string) ([]Version, error) {
-	releases, err := github.FetchReleases(ctx, ghClient, namespace, name)
-	if err != nil {
-		return nil, err
-	}
+func GetVersions(ctx context.Context, ghClient *githubv4.Client, namespace string, name string) (versions []Version, err error) {
+	err = xray.Capture(ctx, "provider.versions", func(tracedCtx context.Context) error {
+		xray.AddAnnotation(tracedCtx, "namespace", namespace)
+		xray.AddAnnotation(tracedCtx, "name", name)
 
-	var versions []Version
-	for _, release := range releases {
-		assets := release.ReleaseAssets.Nodes
-
-		// Extract supported platforms from the release assets.
-		platforms, err := getSupportedArchAndOS(assets)
-		if err != nil {
-			return nil, err
+		releases, releasesErr := github.FetchReleases(tracedCtx, ghClient, namespace, name)
+		if releasesErr != nil {
+			return fmt.Errorf("failed to fetch releases: %w", releasesErr)
 		}
 
-		// Find and parse the manifest associated with the assets.
-		manifest, err := findAndParseManifest(ctx, assets)
-		if err != nil {
-			return nil, err
+		for _, release := range releases {
+			assets := release.ReleaseAssets.Nodes
+
+			// Extract supported platforms from the release assets.
+			platforms, platformsErr := getSupportedArchAndOS(assets)
+			if platformsErr != nil {
+				return fmt.Errorf("failed to get supported platforms: %w", platformsErr)
+			}
+
+			// Find and parse the manifest associated with the assets.
+			manifest, manifestErr := findAndParseManifest(tracedCtx, assets)
+			if manifestErr != nil {
+				return fmt.Errorf("failed to find and parse manifest: %w", manifestErr)
+			}
+
+			// Construct the Version struct.
+			version := Version{
+				// Normalize the version name.
+				Version:   strings.TrimPrefix(release.TagName, "v"),
+				Platforms: platforms,
+			}
+
+			if manifest != nil {
+				version.Protocols = manifest.Metadata.ProtocolVersions
+			} else {
+				version.Protocols = []string{"5.0"}
+			}
+
+			versions = append(versions, version)
 		}
 
-		// Normalize the version name.
-		versionName := release.TagName
-		if strings.HasPrefix(versionName, "v") {
-			versionName = versionName[1:]
-		}
+		return nil
+	})
 
-		// Construct the Version struct.
-		version := Version{
-			Version:   versionName,
-			Platforms: platforms,
-		}
-		if manifest != nil {
-			version.Protocols = manifest.Metadata.ProtocolVersions
-		} else {
-			version.Protocols = []string{"5.0"}
-		}
-
-		versions = append(versions, version)
-	}
-	return versions, nil
+	return
 }
 
 // GetVersion fetches and returns detailed information about a specific version of a provider hosted on GitHub.
@@ -76,61 +81,77 @@ func GetVersions(ctx context.Context, ghClient *githubv4.Client, namespace strin
 //
 // Returns a VersionDetails structure with detailed information about the specified version. If an error occurs during fetching or processing, it returns an error.
 
-func GetVersion(ctx context.Context, ghClient *githubv4.Client, namespace string, name string, version string, OS string, arch string) (*VersionDetails, error) {
-	// Fetch the specific release for the given version.
-	release, err := github.FindRelease(ctx, ghClient, namespace, name, version)
-	if err != nil {
-		return nil, err
-	}
-	if release == nil {
-		return nil, fmt.Errorf("release not found")
-	}
+func GetVersion(ctx context.Context, ghClient *githubv4.Client, namespace string, name string, version string, OS string, arch string) (versionDetails *VersionDetails, err error) {
+	err = xray.Capture(ctx, "provider.versiondetails", func(tracedCtx context.Context) error {
+		xray.AddAnnotation(tracedCtx, "namespace", namespace)
+		xray.AddAnnotation(tracedCtx, "name", name)
+		xray.AddAnnotation(tracedCtx, "version", version)
+		xray.AddAnnotation(tracedCtx, "OS", OS)
+		xray.AddAnnotation(tracedCtx, "arch", arch)
 
-	// Initialize the VersionDetails struct.
-	result := &VersionDetails{
-		OS:   OS,
-		Arch: arch,
-	}
+		// Fetch the specific release for the given version.
+		release, releaseErr := github.FindRelease(tracedCtx, ghClient, namespace, name, version)
+		if releaseErr != nil {
+			return fmt.Errorf("failed to find release: %w", releaseErr)
+		}
 
-	// Find and parse the manifest from the release assets.
-	manifest, err := findAndParseManifest(ctx, release.ReleaseAssets.Nodes)
-	if err != nil {
-		return nil, err
-	}
-	if manifest != nil {
-		result.Protocols = manifest.Metadata.ProtocolVersions
-	} else {
-		result.Protocols = []string{"5.0"}
-	}
+		if release == nil {
+			return fmt.Errorf("release not found")
+		}
 
-	// Identify the appropriate asset for download based on OS and architecture.
-	assetToDownload := github.FindAssetBySuffix(release.ReleaseAssets.Nodes, fmt.Sprintf("_%s_%s.zip", OS, arch))
-	if assetToDownload == nil {
-		return nil, fmt.Errorf("could not find asset to download")
-	}
-	result.Filename = assetToDownload.Name
-	result.DownloadURL = assetToDownload.DownloadURL
+		// Initialize the VersionDetails struct.
+		versionDetails = &VersionDetails{
+			OS:   OS,
+			Arch: arch,
+		}
 
-	// Locate the SHA256 checksums and its signature from the release assets.
-	shaSumsAsset := github.FindAssetBySuffix(release.ReleaseAssets.Nodes, "_SHA256SUMS")
-	shasumsSigAsset := github.FindAssetBySuffix(release.ReleaseAssets.Nodes, "_SHA256SUMS.sig")
-	if shaSumsAsset == nil || shasumsSigAsset == nil {
-		return nil, fmt.Errorf("could not find shasums or its signature asset")
-	}
-	result.SHASumsURL = shaSumsAsset.DownloadURL
-	result.SHASumsSignatureURL = shasumsSigAsset.DownloadURL
+		// Find and parse the manifest from the release assets.
+		manifest, manifestErr := findAndParseManifest(tracedCtx, release.ReleaseAssets.Nodes)
+		if manifestErr != nil {
+			return fmt.Errorf("failed to find and parse manifest: %w", manifestErr)
+		}
 
-	// Extract the SHA256 checksum for the asset to download.
-	shaSum, err := getShaSum(ctx, shaSumsAsset.DownloadURL, result.Filename)
-	if err != nil {
-		return nil, err
-	}
-	result.SHASum = shaSum
+		if manifest != nil {
+			xray.AddAnnotation(tracedCtx, "protocols", fmt.Sprintf("%v", manifest.Metadata.ProtocolVersions))
+			versionDetails.Protocols = manifest.Metadata.ProtocolVersions
+		} else {
+			xray.AddAnnotation(tracedCtx, "protocols", "default")
+			versionDetails.Protocols = []string{"5.0"}
+		}
 
-	// TODO: Handle GPG keys.
-	result.SigningKeys = SigningKeys{
-		GPGPublicKeys: []GPGPublicKey{},
-	}
+		// Identify the appropriate asset for download based on OS and architecture.
+		assetToDownload := github.FindAssetBySuffix(release.ReleaseAssets.Nodes, fmt.Sprintf("_%s_%s.zip", OS, arch))
+		if assetToDownload == nil {
+			return fmt.Errorf("could not find asset to download")
+		}
+		versionDetails.Filename = assetToDownload.Name
+		versionDetails.DownloadURL = assetToDownload.DownloadURL
 
-	return result, nil
+		// Locate the SHA256 checksums and its signature from the release assets.
+		shaSumsAsset := github.FindAssetBySuffix(release.ReleaseAssets.Nodes, "_SHA256SUMS")
+		shasumsSigAsset := github.FindAssetBySuffix(release.ReleaseAssets.Nodes, "_SHA256SUMS.sig")
+
+		if shaSumsAsset == nil || shasumsSigAsset == nil {
+			return fmt.Errorf("could not find shasums or its signature asset")
+		}
+
+		versionDetails.SHASumsURL = shaSumsAsset.DownloadURL
+		versionDetails.SHASumsSignatureURL = shasumsSigAsset.DownloadURL
+
+		// Extract the SHA256 checksum for the asset to download.
+		shaSum, shaSumErr := getShaSum(tracedCtx, shaSumsAsset.DownloadURL, versionDetails.Filename)
+		if shaSumErr != nil {
+			return fmt.Errorf("failed to get shasum: %w", shaSumErr)
+		}
+		versionDetails.SHASum = shaSum
+
+		// TODO: Handle GPG keys.
+		versionDetails.SigningKeys = SigningKeys{
+			GPGPublicKeys: []GPGPublicKey{},
+		}
+
+		return nil
+	})
+
+	return
 }
