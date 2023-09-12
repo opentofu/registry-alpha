8d4645b823960dca3838221f5f6b1c79b668e001
Elbaz <eranelbaz97+github@gmail.com>
2023-09-12 16:52:30 +0300

    add logs
    Signed-off-by: Elbaz <eranelbaz97+github@gmail.com>

--- lambda/moduleVersions.go
+++ lambda/moduleVersions.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 
@@ -36,19 +37,23 @@ func listModuleVersions(config Config) LambdaFunc {
 	return func(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 		params := getListModuleVersionsPathParams(req)
 		repoName := modules.GetRepoName(params.System, params.Name)
-
+		log.Printf("[INFO] Request for %s/%s", params.Namespace, repoName)
 		// check the repo exists
 		exists, err := github.RepositoryExists(ctx, config.ManagedGithubClient, params.Namespace, repoName)
 		if err != nil {
+			log.Printf("[ERROR] Something bad happened - %s", err)
 			return events.APIGatewayProxyResponse{StatusCode: 500}, err
 		}
 		if !exists {
+			log.Printf("[WARN] Did not find such repository %s/%s", params.Namespace, repoName)
 			return NotFoundResponse, nil
 		}
 
 		// fetch all the versions
+		log.Printf("[INFO] Fetching verions for %s/%s", params.Namespace, repoName)
 		versions, err := modules.GetVersions(ctx, config.RawGithubv4Client, params.Namespace, repoName)
 		if err != nil {
+			log.Printf("[ERROR] Something bad happened while fetching versions - %s", err)
 			return events.APIGatewayProxyResponse{StatusCode: 500}, err
 		}
 
@@ -59,9 +64,11 @@ func listModuleVersions(config Config) LambdaFunc {
 				},
 			},
 		}
+		log.Printf("[INFO] Recieved %v versions for %s/%s", len(versions), params.Namespace, repoName)
 
 		resBody, err := json.Marshal(response)
 		if err != nil {
+			log.Printf("[ERROR] Something bad happened while trying to marshel response - %s", err)
 			return events.APIGatewayProxyResponse{StatusCode: 500}, err
 		}
 		return events.APIGatewayProxyResponse{StatusCode: 200, Body: string(resBody)}, nil
