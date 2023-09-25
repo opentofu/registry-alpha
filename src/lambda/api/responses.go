70497a8a622ed2eebab4d15b9bcaa6c5f0ff404b
James Humphries <james@james-humphries.co.uk>
2023-09-25 12:05:26 +0100

    nolint for global variable to be used as a constant
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- src/lambda/api/responses.go
+++ src/lambda/api/responses.go
@@ -2,4 +2,5 @@ package main
 
 import "github.com/aws/aws-lambda-go/events"
 
+//nolint:gochecknoglobals // This should be treated as a constant.
 var NotFoundResponse = events.APIGatewayProxyResponse{StatusCode: 404, Body: `{"errors":["not found"]}`}
