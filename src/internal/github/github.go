9af50b960f9cb86a8b22fa53d8d9504ce7db6b5b
James Humphries <james@james-humphries.co.uk>
2023-09-26 08:58:04 +0100

    ignore the response body on error
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- src/internal/github/github.go
+++ src/internal/github/github.go
@@ -198,7 +198,6 @@ func DownloadAssetContents(ctx context.Context, downloadURL string) (body io.Rea
 
 		resp, respErr := httpClient.Do(req) //nolint:bodyclose // the caller is responsible for closing the body
 		if respErr != nil {
-			resp.Body.Close()
 			return fmt.Errorf("error downloading asset: %w", respErr)
 		}
 
