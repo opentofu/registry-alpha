951823a4e3d70dfaac3f4378327ec3b521ae0611
James Humphries <james@james-humphries.co.uk>
2023-09-29 16:08:05 +0100

    Update lambda timeout for provider version fetching to 10 minutes
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- lambda.tf
+++ lambda.tf
@@ -80,7 +80,7 @@ resource "aws_lambda_function" "populate_provider_versions_function" {
   role          = aws_iam_role.lambda.arn
   handler       = "populate-provider-versions"
   memory_size   = 128
-  timeout       = 60
+  timeout       = 10 * 60
 
   filename         = data.archive_file.populate_provider_versions_archive.output_path
   source_code_hash = data.archive_file.api_function_archive.output_base64sha256
